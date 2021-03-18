534a54ecd060ecaa2b3141971886eb366b5d516b
superdioz <julianus@oxjno.com>
2021-03-18 09:19:36 +0100

    Zencode for the sawroom integration

--- scraper/confirm.php
+++ scraper/confirm.php
@@ -1,4 +1,9 @@
 <?php
+ini_set('max_execution_time', '300');
+require_once ('config.php');
+include_once ('database.php');
+require_once ('api/v1/shared/utilities.php'); 
+
 header("Access-Control-Allow-Origin: *");
 header("Content-Type: application/json; charset=UTF-8");
 header("Access-Control-Allow-Methods: POST");
@@ -6,7 +11,13 @@ header("Access-Control-Max-Age: 3600");
 header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
 
 //TODO: update the database for the records saved on the blockchain
-echo 'allright';
-
+//exit('allright');
+//file_put_contents('payload.txt', file_get_contents("php://input") );
+//$payload = json_decode(stripslashes(file_get_contents("php://input")));
+$payload = file_get_contents("php://input");
+$utilities = new Utilities();
+$result = $utilities->callAPI('POST', 'https://apiroom.net/api/superdioz/Aletheia-StoreInSawroom', '{"data":'.$payload.'}');
+file_put_contents('payload.txt', $payload/*json_encode(array('data'=>$payload, 'keys'=>''))*/);
+file_put_contents('result.txt', $result);
 
 ?>
